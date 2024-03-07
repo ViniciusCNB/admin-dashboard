ad6a02be8a187b69d1fab26652d787c1a15aac80
Vinícius Borges <viniborg25@gmail.com>
2024-03-07 19:57:21 -0300

    Creating the reusable add modal for the pages

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,9 +1,70 @@
+import { useState } from "react"
 import "./products.scss"
+import DataTable from "../../components/dataTable/DataTable"
+import Add from "../../components/add/Add"
+import { GridColDef } from "@mui/x-data-grid"
+import { products } from "../../data"
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Image",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img} alt="" />
+    },
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 250,
+    type: "string",
+  },
+  {
+    field: "color",
+    headerName: "Color",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 150,
+    type: "boolean",
+  },
+]
 
 const Products = () => {
+  const [open, setOpen] = useState(false)
   return (
     <>
-      <div className="products">Products</div>
+      <div className="products">
+        <div className="info">
+          <h1>Products</h1>
+          <button onClick={() => setOpen(true)}>Add new Product</button>
+        </div>
+        <DataTable slug="product" columns={columns} rows={products} />
+        {open && <Add columns={columns} slug="product" setOpen={setOpen} />}
+      </div>
     </>
   )
 }
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,11 +2,13 @@ import { GridColDef } from "@mui/x-data-grid"
 import DataTable from "../../components/dataTable/DataTable"
 import { userRows } from "../../data"
 import "./users.scss"
+import { useState } from "react"
+import Add from "../../components/add/Add"
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "avatar",
+    field: "img",
     headerName: "Avatar",
     width: 100,
     renderCell: (params) => {
@@ -41,6 +43,7 @@ const columns: GridColDef[] = [
     field: "createdAt",
     headerName: "Created At",
     width: 200,
+    type: "string",
   },
   {
     field: "verified",
@@ -51,14 +54,16 @@ const columns: GridColDef[] = [
 ]
 
 const Users = () => {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <div className="users">
         <div className="info">
           <h1>Users</h1>
-          <button>Add new User</button>
+          <button onClick={() => setOpen(true)}>Add new User</button>
         </div>
         <DataTable slug="users" columns={columns} rows={userRows} />
+        {open && <Add columns={columns} slug="user" setOpen={setOpen} />}
       </div>
     </>
   )
