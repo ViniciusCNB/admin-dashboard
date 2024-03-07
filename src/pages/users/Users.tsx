5e2599de8a94baa52e9478800da7bce836ce554a
Vinícius Borges <viniborg25@gmail.com>
2024-03-07 18:42:48 -0300

    Adding MUI datagrid to the Users page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,65 @@
+import { GridColDef } from "@mui/x-data-grid"
+import DataTable from "../../components/dataTable/DataTable"
+import { userRows } from "../../data"
 import "./users.scss"
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+]
 
 const Users = () => {
   return (
     <>
-      <div className="users">Users</div>
+      <div className="users">
+        <div className="info">
+          <h1>Users</h1>
+          <button>Add new User</button>
+        </div>
+        <DataTable slug="users" columns={columns} rows={userRows} />
+      </div>
     </>
   )
 }
