4a06875ea9039878bc92c7d0c71078331944891b
Vinícius Borges <viniborg25@gmail.com>
2024-03-07 23:52:11 -0300

    Creating single pages for user and product

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import Products from "./pages/products/Products"
 import Users from "./pages/users/Users"
+import User from "./pages/user/User"
+import Product from "./pages/product/Product"
 import {
   createBrowserRouter,
   RouterProvider,
@@ -40,19 +42,27 @@ const App = () => {
           element: <Home />,
         },
         {
-          path: "users",
+          path: "/users",
           element: <Users />,
         },
         {
-          path: "products",
+          path: "/products",
           element: <Products />,
         },
-      ]
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
     },
     {
       path: "/login",
       element: <Login />,
-    }
+    },
   ])
 
   return (
